fc70954ba3a72e0179b1d889b3ff31d8e2eb03fe
Joe Thackery <jthackery@eaze.com>
2020-09-23 16:15:28 -0700

    add test workflow, add linting, install missing mocha, update lodash

--- index-test.js
+++ index-test.js
@@ -5,7 +5,7 @@ const casingUtil = require('./index')
 
 describe('toCamelCase', () => {
   it('should convert properties from any case to camelCase', done => {
-    const obj = {foo_bar: 'bazz'}
+    const obj = { foo_bar: 'bazz' }
     const obj1 = casingUtil.toCamelCase(obj)
     expect(obj1.fooBar).to.equal('bazz')
     expect(obj.foo_bar).to.equal('bazz')
@@ -15,7 +15,7 @@ describe('toCamelCase', () => {
 
 describe('toSnakeCase', () => {
   it('should convert top level properties from camelCase to snake_case', done => {
-    const obj = {fooBar: 'bazz'}
+    const obj = { fooBar: 'bazz' }
     const obj1 = casingUtil.toSnakeCase(obj)
     expect(obj1.foo_bar).to.equal('bazz')
     expect(obj.fooBar).to.equal('bazz')
